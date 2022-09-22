077de36fde5c075685839cc7ea4b1005805ac80a
Darren Reid <darren@reidmail.org>
2022-09-22 14:20:36 +1000

    Update generation workflow.

--- generate.sh
+++ generate.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-cd ..
-dotnet build
 cd docs
 docfx metadata
 find ./api/*.yml -type f -exec sed -i "s|git@github.com:ServiceStack/ServiceStack.git|https://github.com/ServiceStack/ServiceStack|g" {} \;
