ed5dd573a6050b543cc62ccb15de2aaa5e51957e
Darren Reid <darren@reidmail.org>
2022-09-22 17:32:28 +1000

    Debug yml on CI.

--- generate.sh
+++ generate.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 docfx.exe metadata
+cat ./api/ServiceStack.Blazor.BlazorClient.yml
 find ./api/*.yml -type f -exec sed -i "s|git@github.com:ServiceStack/ServiceStack.git|https://github.com/ServiceStack/ServiceStack|g" {} \;
 dfmg
 yarn install
