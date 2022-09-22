a93485b9d9382cd7e5ebd382e8c788487457d90f
Darren Reid <darren@reidmail.org>
2022-09-22 15:27:00 +1000

    Update generation workflow.

--- generate.sh
+++ generate.sh
@@ -3,3 +3,5 @@
 docfx.exe metadata
 find ./api/*.yml -type f -exec sed -i "s|git@github.com:ServiceStack/ServiceStack.git|https://github.com/ServiceStack/ServiceStack|g" {} \;
 dfmg
+yarn install
+npm run build
