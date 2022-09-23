3fdcd4a9ec5de94b6c71da412a574c1bca74d940
Darren Reid <darren@reidmail.org>
2022-09-23 16:13:44 +1000

    Try switch to use Powershell with windows 2022.

---
+++ generate.ps1
@@ -0,0 +1,16 @@
+docfx metadata
+Get-ChildItem './api/*.yml' | ForEach {
+    (Get-Content $_) | ForEach {$_ -Replace 'git@github.com:ServiceStack/ServiceStack.git', 'https://github.com/ServiceStack/ServiceStack'} | Set-Content $_
+}
+if (Test-Path -Path './DocFxMarkdownGen') {
+    echo "Already exists, skipping DocFXMarkdownGen clone"
+} else {
+    git clone git@github.com:Layoric/DocFxMarkdownGen.git
+}
+cd DocFxMarkdownGen
+dotnet publish -r win-x86 -p:PublishSingleFile=true --self-contained false -o ../out
+cd ..
+Copy-Item "./out/DocFxMarkdownGen.exe" -Destination "./dfmg.exe"
+./dfmg.exe
+yarn install
+npm run build
