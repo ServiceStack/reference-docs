161147a10a13b225ef3c954161184316c18923a4
Darren Reid <darren@reidmail.org>
2022-09-23 14:46:51 +1000

    Update process to be more easily repeatable locally.

--- generate.sh
+++ generate.sh
@@ -1,8 +1,25 @@
 #!/bin/sh
 
 docfx.exe metadata
-cat ./api/ServiceStack.Blazor.BlazorClient.yml
 find ./api/*.yml -type f -exec sed -i "s|git@github.com:ServiceStack/ServiceStack.git|https://github.com/ServiceStack/ServiceStack|g" {} \;
-DocFxMarkdownGen
+if [ ! -d ./DocFxMarkdownGen ]; then
+  git clone git@github.com:Layoric/DocFxMarkdownGen.git
+fi
+unameOut="$(uname -s)"
+
+case "${unameOut}" in
+    Linux*)     machine=linux-x64;;
+    CYGWIN*)    machine=win-x86;;
+    MINGW*)     machine=win-x86;;
+    *)          machine=linux-x64
+esac
+echo ${machine}
+cd DocFxMarkdownGen
+dotnet publish -r ${machine} -p:PublishSingleFile=true --self-contained false -o ../out
+cd ..
+chmod +x ./out/DocFxMarkdownGen
+cp ./out/DocFxMarkdownGen ./dfmg
+chmod +x ./dfmg
+./dfmg
 yarn install
 npm run build
